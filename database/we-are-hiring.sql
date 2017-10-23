5522dc6e83a9481309afa7b29b37a12cf4ee9859
Oskar Joelson <oskar.joelson@hoy.se>
2017-10-23 14:19:11 +0200

    Run wp install with .dev instead

--- database/we-are-hiring.sql
+++ database/we-are-hiring.sql
@@ -84,109 +84,6 @@ LOCK TABLES `wpl_comments` WRITE;
 UNLOCK TABLES;
 
 --
--- Table structure for table `wpl_itsec_lockouts`
---
-
-DROP TABLE IF EXISTS `wpl_itsec_lockouts`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `wpl_itsec_lockouts` (
-  `lockout_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `lockout_type` varchar(20) COLLATE utf8mb4_unicode_520_ci NOT NULL,
-  `lockout_start` datetime NOT NULL,
-  `lockout_start_gmt` datetime NOT NULL,
-  `lockout_expire` datetime NOT NULL,
-  `lockout_expire_gmt` datetime NOT NULL,
-  `lockout_host` varchar(40) COLLATE utf8mb4_unicode_520_ci DEFAULT NULL,
-  `lockout_user` bigint(20) unsigned DEFAULT NULL,
-  `lockout_username` varchar(60) COLLATE utf8mb4_unicode_520_ci DEFAULT NULL,
-  `lockout_active` int(1) NOT NULL DEFAULT '1',
-  PRIMARY KEY (`lockout_id`),
-  KEY `lockout_expire_gmt` (`lockout_expire_gmt`),
-  KEY `lockout_host` (`lockout_host`),
-  KEY `lockout_user` (`lockout_user`),
-  KEY `lockout_username` (`lockout_username`),
-  KEY `lockout_active` (`lockout_active`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `wpl_itsec_lockouts`
---
-
-LOCK TABLES `wpl_itsec_lockouts` WRITE;
-/*!40000 ALTER TABLE `wpl_itsec_lockouts` DISABLE KEYS */;
-/*!40000 ALTER TABLE `wpl_itsec_lockouts` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `wpl_itsec_log`
---
-
-DROP TABLE IF EXISTS `wpl_itsec_log`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `wpl_itsec_log` (
-  `log_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `log_type` varchar(20) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT '',
-  `log_function` varchar(255) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT '',
-  `log_priority` int(2) NOT NULL DEFAULT '1',
-  `log_date` datetime NOT NULL DEFAULT '1000-01-01 00:00:00',
-  `log_date_gmt` datetime NOT NULL DEFAULT '1000-01-01 00:00:00',
-  `log_host` varchar(40) COLLATE utf8mb4_unicode_520_ci DEFAULT NULL,
-  `log_username` varchar(60) COLLATE utf8mb4_unicode_520_ci DEFAULT NULL,
-  `log_user` bigint(20) unsigned DEFAULT NULL,
-  `log_url` varchar(255) COLLATE utf8mb4_unicode_520_ci DEFAULT NULL,
-  `log_referrer` varchar(255) COLLATE utf8mb4_unicode_520_ci DEFAULT NULL,
-  `log_data` longtext COLLATE utf8mb4_unicode_520_ci NOT NULL,
-  PRIMARY KEY (`log_id`),
-  KEY `log_type` (`log_type`),
-  KEY `log_date_gmt` (`log_date_gmt`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `wpl_itsec_log`
---
-
-LOCK TABLES `wpl_itsec_log` WRITE;
-/*!40000 ALTER TABLE `wpl_itsec_log` DISABLE KEYS */;
-/*!40000 ALTER TABLE `wpl_itsec_log` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `wpl_itsec_temp`
---
-
-DROP TABLE IF EXISTS `wpl_itsec_temp`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `wpl_itsec_temp` (
-  `temp_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `temp_type` varchar(20) COLLATE utf8mb4_unicode_520_ci NOT NULL,
-  `temp_date` datetime NOT NULL,
-  `temp_date_gmt` datetime NOT NULL,
-  `temp_host` varchar(40) COLLATE utf8mb4_unicode_520_ci DEFAULT NULL,
-  `temp_user` bigint(20) unsigned DEFAULT NULL,
-  `temp_username` varchar(60) COLLATE utf8mb4_unicode_520_ci DEFAULT NULL,
-  PRIMARY KEY (`temp_id`),
-  KEY `temp_date_gmt` (`temp_date_gmt`),
-  KEY `temp_host` (`temp_host`),
-  KEY `temp_user` (`temp_user`),
-  KEY `temp_username` (`temp_username`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `wpl_itsec_temp`
---
-
-LOCK TABLES `wpl_itsec_temp` WRITE;
-/*!40000 ALTER TABLE `wpl_itsec_temp` DISABLE KEYS */;
-/*!40000 ALTER TABLE `wpl_itsec_temp` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `wpl_links`
 --
 
@@ -235,7 +132,7 @@ CREATE TABLE `wpl_options` (
   `autoload` varchar(20) COLLATE utf8mb4_unicode_520_ci NOT NULL DEFAULT 'yes',
   PRIMARY KEY (`option_id`),
   UNIQUE KEY `option_name` (`option_name`)
-) ENGINE=InnoDB AUTO_INCREMENT=125 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=100 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -244,8 +141,8 @@ CREATE TABLE `wpl_options` (
 
 LOCK TABLES `wpl_options` WRITE;
 /*!40000 ALTER TABLE `wpl_options` DISABLE KEYS */;
-INSERT INTO `wpl_options` VALUES (1,'siteurl','http://we-are-hiring.se/wordpress','yes'),
-(2,'home','http://we-are-hiring.se/wordpress','yes'),
+INSERT INTO `wpl_options` VALUES (1,'siteurl','http://we-are-hiring.dev/wordpress','yes'),
+(2,'home','http://we-are-hiring.dev/wordpress','yes'),
 (3,'blogname','We Are Hiring','yes'),
 (4,'blogdescription','Just another WordPress site','yes'),
 (5,'users_can_register','0','yes'),
@@ -271,7 +168,7 @@ INSERT INTO `wpl_options` VALUES (1,'siteurl','http://we-are-hiring.se/wordpress
 (25,'links_updated_date_format','F j, Y g:i a','yes'),
 (26,'comment_moderation','0','yes'),
 (27,'moderation_notify','1','yes'),
-(28,'permalink_structure','/%postname%/','yes'),
+(28,'permalink_structure','','yes'),
 (29,'rewrite_rules','','yes'),
 (30,'hack_file','0','yes'),
 (31,'blog_charset','UTF-8','yes'),
@@ -324,7 +221,7 @@ INSERT INTO `wpl_options` VALUES (1,'siteurl','http://we-are-hiring.se/wordpress
 (78,'widget_categories','a:2:{i:2;a:4:{s:5:\"title\";s:0:\"\";s:5:\"count\";i:0;s:12:\"hierarchical\";i:0;s:8:\"dropdown\";i:0;}s:12:\"_multiwidget\";i:1;}','yes'),
 (79,'widget_text','a:0:{}','yes'),
 (80,'widget_rss','a:0:{}','yes'),
-(81,'uninstall_plugins','a:1:{s:41:\"better-wp-security/better-wp-security.php\";a:2:{i:0;s:10:\"ITSEC_Core\";i:1;s:16:\"handle_uninstall\";}}','no'),
+(81,'uninstall_plugins','a:0:{}','no'),
 (82,'timezone_string','','yes'),
 (83,'page_for_posts','0','yes'),
 (84,'page_on_front','0','yes'),
@@ -342,21 +239,7 @@ INSERT INTO `wpl_options` VALUES (1,'siteurl','http://we-are-hiring.se/wordpress
 (96,'widget_recent-comments','a:2:{i:2;a:2:{s:5:\"title\";s:0:\"\";s:6:\"number\";i:5;}s:12:\"_multiwidget\";i:1;}','yes'),
 (97,'widget_archives','a:2:{i:2;a:3:{s:5:\"title\";s:0:\"\";s:5:\"count\";i:0;s:8:\"dropdown\";i:0;}s:12:\"_multiwidget\";i:1;}','yes'),
 (98,'widget_meta','a:2:{i:2;a:1:{s:5:\"title\";s:0:\"\";}s:12:\"_multiwidget\";i:1;}','yes'),
-(99,'sidebars_widgets','a:5:{s:19:\"wp_inactive_widgets\";a:0:{}s:9:\"sidebar-1\";a:6:{i:0;s:8:\"search-2\";i:1;s:14:\"recent-posts-2\";i:2;s:17:\"recent-comments-2\";i:3;s:10:\"archives-2\";i:4;s:12:\"categories-2\";i:5;s:6:\"meta-2\";}s:9:\"sidebar-2\";a:0:{}s:9:\"sidebar-3\";a:0:{}s:13:\"array_version\";i:3;}','yes'),
-(100,'wordplate_autoloader_plugin_cache','a:1:{s:5:\"files\";a:5:{i:0;s:33:\"disable-embeds/disable-embeds.php\";i:1;s:33:\"disable-emojis/disable-emojis.php\";i:2;s:41:\"better-wp-security/better-wp-security.php\";i:3;s:27:\"normalizer/tl-normalize.php\";i:4;s:15:\"plate/plate.php\";}}','no'),
-(101,'widget_pages','a:1:{s:12:\"_multiwidget\";i:1;}','yes'),
-(102,'widget_calendar','a:1:{s:12:\"_multiwidget\";i:1;}','yes'),
-(103,'widget_media_audio','a:1:{s:12:\"_multiwidget\";i:1;}','yes'),
-(104,'widget_media_image','a:1:{s:12:\"_multiwidget\";i:1;}','yes'),
-(105,'widget_media_video','a:1:{s:12:\"_multiwidget\";i:1;}','yes'),
-(106,'widget_tag_cloud','a:1:{s:12:\"_multiwidget\";i:1;}','yes'),
-(107,'widget_nav_menu','a:1:{s:12:\"_multiwidget\";i:1;}','yes'),
-(108,'widget_custom_html','a:1:{s:12:\"_multiwidget\";i:1;}','yes'),
-(109,'cron','a:3:{i:1508804128;a:3:{s:16:\"wp_version_check\";a:1:{s:32:\"40cd750bba9870f18aada2478b24840a\";a:3:{s:8:\"schedule\";s:10:\"twicedaily\";s:4:\"args\";a:0:{}s:8:\"interval\";i:43200;}}s:17:\"wp_update_plugins\";a:1:{s:32:\"40cd750bba9870f18aada2478b24840a\";a:3:{s:8:\"schedule\";s:10:\"twicedaily\";s:4:\"args\";a:0:{}s:8:\"interval\";i:43200;}}s:16:\"wp_update_themes\";a:1:{s:32:\"40cd750bba9870f18aada2478b24840a\";a:3:{s:8:\"schedule\";s:10:\"twicedaily\";s:4:\"args\";a:0:{}s:8:\"interval\";i:43200;}}}i:1508847328;a:3:{s:16:\"itsec_purge_logs\";a:1:{s:32:\"40cd750bba9870f18aada2478b24840a\";a:3:{s:8:\"schedule\";s:5:\"daily\";s:4:\"args\";a:0:{}s:8:\"interval\";i:86400;}}s:20:\"itsec_purge_lockouts\";a:1:{s:32:\"40cd750bba9870f18aada2478b24840a\";a:3:{s:8:\"schedule\";s:5:\"daily\";s:4:\"args\";a:0:{}s:8:\"interval\";i:86400;}}s:17:\"itsec_clear_locks\";a:1:{s:32:\"40cd750bba9870f18aada2478b24840a\";a:3:{s:8:\"schedule\";s:5:\"daily\";s:4:\"args\";a:0:{}s:8:\"interval\";i:86400;}}}s:7:\"version\";i:2;}','yes'),
-(112,'itsec_active_modules','a:9:{s:9:\"ban-users\";b:1;s:6:\"backup\";b:0;s:11:\"brute-force\";b:1;s:19:\"network-brute-force\";b:1;s:16:\"strong-passwords\";b:1;s:16:\"wordpress-tweaks\";b:1;s:13:\"404-detection\";b:1;s:11:\"file-change\";b:1;s:13:\"system-tweaks\";b:1;}','yes'),
-(113,'itsec-storage','a:7:{s:16:\"wordpress-tweaks\";a:12:{s:18:\"wlwmanifest_header\";b:0;s:14:\"edituri_header\";b:0;s:12:\"comment_spam\";b:0;s:11:\"file_editor\";b:0;s:14:\"disable_xmlrpc\";i:0;s:22:\"allow_xmlrpc_multiauth\";b:0;s:8:\"rest_api\";s:14:\"default-access\";s:12:\"login_errors\";b:0;s:21:\"force_unique_nicename\";b:0;s:27:\"disable_unused_author_pages\";b:0;s:16:\"block_tabnapping\";b:0;s:21:\"valid_user_login_type\";s:4:\"both\";}s:6:\"global\";a:31:{s:18:\"notification_email\";a:1:{i:0;s:11:\"tech@sot.se\";}s:12:\"backup_email\";a:1:{i:0;s:11:\"tech@sot.se\";}s:15:\"lockout_message\";s:5:\"error\";s:20:\"user_lockout_message\";s:64:\"You have been locked out due to too many invalid login attempts.\";s:25:\"community_lockout_message\";s:77:\"Your IP address has been flagged as a threat by the iThemes Security network.\";s:9:\"blacklist\";b:1;s:15:\"blacklist_count\";i:3;s:16:\"blacklist_period\";i:7;s:19:\"email_notifications\";b:0;s:14:\"lockout_period\";i:15;s:18:\"lockout_white_list\";a:1:{i:0;s:14:\"213.136.62.178\";}s:12:\"log_rotation\";i:14;s:8:\"log_type\";s:8:\"database\";s:12:\"log_location\";s:96:\"D:\\dokument\\projekt\\sot\\We Are More\\hiring\\we-are-hiring-wp\\public/uploads/ithemes-security/logs\";s:8:\"log_info\";s:44:\"we-are-hiring-JcLzmCouUAEb0cvv6fGrlJVkfYPSUI\";s:14:\"allow_tracking\";b:0;s:11:\"write_files\";b:1;s:10:\"nginx_file\";s:87:\"D:\\dokument\\projekt\\sot\\We Are More\\hiring\\we-are-hiring-wp\\public\\wordpress/nginx.conf\";s:24:\"infinitewp_compatibility\";b:0;s:11:\"did_upgrade\";b:0;s:9:\"lock_file\";b:0;s:12:\"digest_email\";b:0;s:14:\"proxy_override\";b:0;s:14:\"hide_admin_bar\";b:0;s:16:\"show_error_codes\";b:0;s:25:\"show_new_dashboard_notice\";b:1;s:19:\"show_security_check\";b:1;s:16:\"digest_last_sent\";i:1508760928;s:15:\"digest_messages\";a:0:{}s:5:\"build\";i:4075;s:20:\"activation_timestamp\";i:0;}s:9:\"ban-users\";a:4:{s:7:\"default\";b:1;s:16:\"enable_ban_lists\";b:1;s:9:\"host_list\";a:0:{}s:10:\"agent_list\";a:0:{}}s:11:\"file-change\";a:10:{s:5:\"split\";b:0;s:6:\"method\";s:7:\"exclude\";s:9:\"file_list\";a:0:{}s:5:\"types\";a:7:{i:0;s:4:\".jpg\";i:1;s:5:\".jpeg\";i:2;s:4:\".png\";i:3;s:4:\".log\";i:4;s:3:\".mo\";i:5;s:3:\".po\";i:6;s:4:\".pdf\";}s:5:\"email\";b:0;s:12:\"notify_admin\";b:1;s:8:\"last_run\";i:1508674708;s:10:\"last_chunk\";b:0;s:12:\"show_warning\";b:0;s:14:\"latest_changes\";a:0:{}}s:19:\"network-brute-force\";a:5:{s:7:\"api_key\";s:32:\"pBY304rg8Fqg7JWIT6SL1576MHX1PaIK\";s:10:\"api_secret\";s:128:\"3G52yVN4u7Ytb8EK9562vyBPGUFrdpV5i248317L3S78p4xcut25kO8j6986Y119Vs3t4fVV8q3C60j4U9DZku2fr9n78SlhKZJFbupl936lj4HT0yobw06iI520u89c\";s:10:\"enable_ban\";b:1;s:13:\"updates_optin\";b:1;s:7:\"api_nag\";b:0;}s:13:\"system-tweaks\";a:10:{s:13:\"protect_files\";b:1;s:18:\"directory_browsing\";b:1;s:15:\"request_methods\";b:0;s:24:\"suspicious_query_strings\";b:1;s:22:\"non_english_characters\";b:0;s:16:\"long_url_strings\";b:0;s:17:\"write_permissions\";b:1;s:11:\"uploads_php\";b:1;s:10:\"themes_php\";b:0;s:11:\"plugins_php\";b:0;}s:12:\"hide-backend\";a:6:{s:7:\"enabled\";b:1;s:4:\"slug\";s:9:\"sot-admin\";s:8:\"register\";s:13:\"wp-signup.php\";s:12:\"theme_compat\";b:0;s:17:\"theme_compat_slug\";s:9:\"not_found\";s:16:\"post_logout_slug\";s:0:\"\";}}','yes'),
-(116,'itsec_temp_whitelist_ip','a:1:{s:9:\"127.0.0.1\";i:1508847344;}','no'),
-(124,'can_compress_scripts','1','no');
+(99,'sidebars_widgets','a:5:{s:19:\"wp_inactive_widgets\";a:0:{}s:9:\"sidebar-1\";a:6:{i:0;s:8:\"search-2\";i:1;s:14:\"recent-posts-2\";i:2;s:17:\"recent-comments-2\";i:3;s:10:\"archives-2\";i:4;s:12:\"categories-2\";i:5;s:6:\"meta-2\";}s:9:\"sidebar-2\";a:0:{}s:9:\"sidebar-3\";a:0:{}s:13:\"array_version\";i:3;}','yes');
 /*!40000 ALTER TABLE `wpl_options` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -560,7 +443,7 @@ CREATE TABLE `wpl_usermeta` (
   PRIMARY KEY (`umeta_id`),
   KEY `user_id` (`user_id`),
   KEY `meta_key` (`meta_key`(191))
-) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -582,10 +465,7 @@ INSERT INTO `wpl_usermeta` VALUES (1,1,'nickname','sot'),
 (11,1,'wpl_capabilities','a:1:{s:13:\"administrator\";b:1;}'),
 (12,1,'wpl_user_level','10'),
 (13,1,'dismissed_wp_pointers',''),
-(14,1,'show_welcome_panel','1'),
-(15,1,'session_tokens','a:1:{s:64:\"cdb7c865cf7a986caba9a765c00f7597f9350d3792a8a47b9b162a1b5b3b81d5\";a:4:{s:10:\"expiration\";i:1509970543;s:2:\"ip\";s:9:\"127.0.0.1\";s:2:\"ua\";s:78:\"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:56.0) Gecko/20100101 Firefox/56.0\";s:5:\"login\";i:1508760943;}}'),
-(16,1,'itsec_user_activity_last_seen','1508760945'),
-(17,1,'itsec-settings-view','grid');
+(14,1,'show_welcome_panel','1');
 /*!40000 ALTER TABLE `wpl_usermeta` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -620,7 +500,7 @@ CREATE TABLE `wpl_users` (
 
 LOCK TABLES `wpl_users` WRITE;
 /*!40000 ALTER TABLE `wpl_users` DISABLE KEYS */;
-INSERT INTO `wpl_users` VALUES (1,'sot','$2y$10$kpyV5Me03d197aK5d3Mja.m..y6mmb1Z6zh7STFTpxdMRPOB3J1mG','sot','service@sot.se','','2017-10-23 12:15:26','',0,'sot');
+INSERT INTO `wpl_users` VALUES (1,'sot','$2y$10$0bFXaM6Lv4Fb2vrdbMh1hu/QbMZl6Wh/XhNjMhVtMNpGnQQd41rnm','sot','service@sot.se','','2017-10-23 12:18:52','',0,'sot');
 /*!40000 ALTER TABLE `wpl_users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -633,4 +513,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-10-23 13:16:11
+-- Dump completed on 2017-10-23 13:19:02

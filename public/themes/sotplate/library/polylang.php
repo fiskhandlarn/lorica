cd576506fda5af191ceb2268a4ea471ef9d394a9
Oskar Joelson <oskar.joelson@hoy.se>
2018-03-05 11:41:48 +0100

    Remove support for localized strftime (use IntlDateFormatter instead)

--- public/themes/sotplate/library/polylang.php
+++ public/themes/sotplate/library/polylang.php
@@ -19,29 +19,3 @@ if (!function_exists('trans')) {
         return __($key);
     }
 }
-
-// add support for localized strftime()
-if (function_exists('pll_current_language')) {
-    add_action( 'init', function() {
-        $polylangLocale = pll_current_language('locale');
-
-        // https://stackoverflow.com/a/5879078/1109380
-        if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
-
-            // TODO automate this
-            // http://se2.php.net/manual/en/function.setlocale.php#refsect1-function.setlocale-notes ->
-            // https://msdn.microsoft.com/en-us/library/39cwe7zf%28v=vs.90%29.aspx
-            switch ($polylangLocale) {
-            case "sv_SE":
-                $polylangLocale = "swedish";
-                break;
-            default:
-                $polylangLocale = "english";
-            }
-        } else {
-            $polylangLocale .= ".utf8";
-        }
-
-        setlocale(LC_ALL, $polylangLocale);
-    });
-}

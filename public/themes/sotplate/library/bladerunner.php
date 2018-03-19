2287a1b77766c92b3d903cd7d4d40423cf57296f
Oskar Joelson <oskar.joelson@hoy.se>
2018-03-19 11:00:09 +0100

    Add comments

--- public/themes/sotplate/library/bladerunner.php
+++ public/themes/sotplate/library/bladerunner.php
@@ -6,6 +6,7 @@ if (!defined('ABSPATH')) {
     exit();
 }
 
+// Make bladerunner look in resources/ instead of in the theme folder (Laravel style)
 add_filter('bladerunner/template/bladepath', function ($paths) {
     if (!is_array($paths)) {
         $paths = [$paths];
@@ -14,10 +15,12 @@ add_filter('bladerunner/template/bladepath', function ($paths) {
     return $paths;
 });
 
+// Make bladerunner save cached files in storage/ instead of in the theme folder (Laravel style)
 add_filter('bladerunner/cache/path', function ($path) {
     return base_path('storage/views');
 });
 
+// Don't create the cache directory (this should instead be handled via storage/views/.gitignore)
 add_filter('bladerunner/cache/make', function () {
     return false;
 });

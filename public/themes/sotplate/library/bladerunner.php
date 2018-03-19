a05a9be787360ae560d730cd3b651336fc2c5a6c
Oskar Joelson <oskar.joelson@hoy.se>
2018-03-19 10:56:17 +0100

    Use wordplate's base_path instead of ABSPATH, run php-cs-fixer

--- public/themes/sotplate/library/bladerunner.php
+++ public/themes/sotplate/library/bladerunner.php
@@ -2,21 +2,23 @@
 
 declare(strict_types=1);
 
-if (!defined('ABSPATH')) { exit(); }
+if (!defined('ABSPATH')) {
+    exit();
+}
 
 add_filter('bladerunner/template/bladepath', function ($paths) {
     if (!is_array($paths)) {
         $paths = [$paths];
     }
-    $paths[] = ABSPATH . '../../resources/views';
+    $paths[] = base_path('resources/views');
     return $paths;
 });
 
-add_filter('bladerunner/cache/path', function($path) {
-	return ABSPATH . '../../storage/views';
+add_filter('bladerunner/cache/path', function ($path) {
+    return base_path('storage/views');
 });
 
-add_filter('bladerunner/cache/make', function() {
+add_filter('bladerunner/cache/make', function () {
     return false;
 });
 

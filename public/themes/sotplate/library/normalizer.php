04c0691660579f43130622196850cfdd03d36a10
Oskar Joelson <oskar.joelson@hoy.se>
2018-03-19 16:10:20 +0100

    Run php-cs-fixer fix

--- public/themes/sotplate/library/normalizer.php
+++ public/themes/sotplate/library/normalizer.php
@@ -2,14 +2,16 @@
 
 declare(strict_types=1);
 
-if (!defined('ABSPATH')) { exit(); }
+if (!defined('ABSPATH')) {
+    exit();
+}
 
 global $normalizer;
 
-if ( isset( $normalizer ) && $normalizer->compatible_version() ) {
+if (isset($normalizer) && $normalizer->compatible_version()) {
     // also normalize acf fields
     add_filter('acf/update_value', function ($value) use ($normalizer) {
-       if (is_string($value)) {
+        if (is_string($value)) {
             return $normalizer->tl_normalizer($value);
         }
 

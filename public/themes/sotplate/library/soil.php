06cf1fed5dadc99af2b22e3297ca96fdca75248c
Oskar Joelson <oskar.joelson@sot.se>
2018-04-13 11:30:28 +0200

    Do add_theme_support calls without the after_setup_theme action

--- public/themes/sotplate/library/soil.php
+++ public/themes/sotplate/library/soil.php
@@ -8,14 +8,12 @@ if (!defined('ABSPATH')) {
 
 // configure roots/soil
 // https://github.com/roots/soil
-add_action('after_setup_theme', function () {
-    add_theme_support('soil-clean-up');
-    add_theme_support('soil-disable-trackbacks');
-    //add_theme_support('soil-google-analytics', 'UA-XXXXX-Y', 'wp_head');
-    add_theme_support('soil-js-to-footer');
-    //add_theme_support('soil-nav-walker');
-    add_theme_support('soil-nice-search');
-});
+add_theme_support('soil-clean-up');
+add_theme_support('soil-disable-trackbacks');
+//add_theme_support('soil-google-analytics', 'UA-XXXXX-Y', 'wp_head');
+add_theme_support('soil-js-to-footer');
+//add_theme_support('soil-nav-walker');
+add_theme_support('soil-nice-search');
 
 function google_analytics_deploy($trackingID)
 {

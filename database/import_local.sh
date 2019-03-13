87e72f90de02680e1cd10f88a44e1c9bfa2e3c9f
Oskar Joelson <oskar.joelson@sot.se>
2019-03-13 09:49:26 +0100

    Assert DB_NAME and BROWSER_SYNC_HOST before using them

--- database/import_local.sh
+++ database/import_local.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 SOURCEDIR="$( cd "$( dirname "$0" )" && pwd )"
 
-cd "$SOURCEDIR"
-"./_import.sh" $(wp eval "echo env('DB_NAME');").sql https://$(wp eval "echo env('BROWSER_SYNC_HOST');")
+database=$(wp eval "echo env('DB_NAME');")
+url=$(wp eval "echo env('BROWSER_SYNC_HOST');")
+
+if [ "$database" = "" ]; then
+  echo "DB_NAME not set in .env"
+elif [ "$url" = "" ]; then
+  echo "BROWSER_SYNC_HOST not set in .env"
+else
+  cd "$SOURCEDIR"
+  "./_import.sh" "$database.sql" "https://$url"
+fi

d4f71c420ce360074d5ca8441c5eee1d21a8e543
Oskar Joelson <oskar.joelson@sot.se>
2019-03-13 10:48:31 +0100

    Revert to db export instead
    Issue: search-replace export is slow on large databases

--- database/_export.sh
+++ database/_export.sh
@@ -16,12 +16,32 @@ then
   cd "$SOURCEDIR"
 
   echo ""
+  echo "Replacing $old to $new in database"
+  "$wp" search-replace $old $new --all-tables --report-changed-only
+
+  echo ""
   echo "Cleaning up"
   "$wp" db query < cleanup.sql
 
   echo ""
-  echo "Replacing $old to $new in database and exporting to $DESTINATION_SQL"
-  "$wp" search-replace $old $new --all-tables --report-changed-only --export="$DESTINATION_SQL"
+  echo "Exporting database to $DESTINATION_SQL"
+  "$wp" db export "$DESTINATION_SQL"
+
+  echo ""
+  echo "Add newlines in $DESTINATION_SQL"
+  sed -i 's$),($),\
+($g' "$DESTINATION_SQL"
+
+  if [ -x "$(command -v fromdos)" ]
+  then
+    echo ""
+    echo "Convert line endings in $DESTINATION_SQL"
+    fromdos "$(cygpath -w "$SOURCEDIR")\\$DESTINATION_SQL"
+  fi
+
+  echo ""
+  echo "Restoring $new to $old in database"
+  "$wp" search-replace $new $old --all-tables --report-changed-only
 else
   echo "Expected at least two parameters";
 fi

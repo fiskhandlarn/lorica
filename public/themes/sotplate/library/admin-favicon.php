834ab1e433cf71d595e1b185edf637ca5a300e98
Oskar Joelson <oskar.joelson@sot.se>
2019-03-13 15:05:04 +0100

    Set admin favicon on login page too

--- public/themes/sotplate/library/admin-favicon.php
+++ public/themes/sotplate/library/admin-favicon.php
@@ -6,6 +6,9 @@ if (!defined('ABSPATH')) {
     exit();
 }
 
-add_action('admin_head', function () {
+add_action('admin_head', 'sotplate_admin_favicon');
+add_action('login_head', 'sotplate_admin_favicon');
+function sotplate_admin_favicon()
+{
     echo '<link rel="icon" href="' . image_url('favicons/favicon.admin.png') . '" type="image/png" />' . "\n";
-});
+}

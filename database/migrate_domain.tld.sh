27cd74c92d0052775644890475b351c943ef9433
Oskar Joelson <oskar.joelson@sot.se>
2019-03-20 14:46:18 +0100

    Trim wp eval

--- database/migrate_domain.tld.sh
+++ database/migrate_domain.tld.sh
@@ -2,4 +2,4 @@
 SOURCEDIR="$( cd "$( dirname "$0" )" && pwd )"
 
 cd "$SOURCEDIR"
-"./_migrate.sh" domain.tld https://$(wp eval "echo env('BROWSER_SYNC_HOST');") https://domain.tld
+"./_migrate.sh" domain.tld https://$(wp eval "echo env('BROWSER_SYNC_HOST');"|xargs) https://domain.tld

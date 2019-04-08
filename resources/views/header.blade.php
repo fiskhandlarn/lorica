e3712b23a94fbf8d2d9ea59f64f7affcf625c3ea
Oskar Joelson <oskar.joelson@sot.se>
2019-04-08 11:51:08 +0200

    Upgrade wordplate/framework to 7.1.0

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html {!! get_language_attributes() !!}>
 <head>
-  <meta charset="{{ info('charset') }}">
+  <meta charset="{{ get_bloginfo('charset') }}">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
   @include('partials/favicons')

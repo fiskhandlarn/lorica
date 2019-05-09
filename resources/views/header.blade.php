bbbbe9702db4ac6484b4d8ec758dc2d3ef448d34
Oskar Joelson <oskar.joelson@sot.se>
2019-05-09 13:17:35 +0200

    Revert accidental commit

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -4,8 +4,6 @@
   <meta charset="{{ get_bloginfo('charset') }}">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-
   @include('partials/favicons')
 
   <meta name="theme-color" content="#0072bc">
